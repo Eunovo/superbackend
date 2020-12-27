bb7cea1ec715e6428130eab2d8fe18af41cde52b
Eunovo <eunovo9@gmail.com>
2020-12-27 21:38:47 +0100

    Set some todo tests

--- packages/main/test/services.test.ts
+++ packages/main/test/services.test.ts
@@ -4,5 +4,18 @@ import { buildServices } from "../src";
 const schemaPath = "./test/mock.graphql";
 
 test.todo("it should create CRUD services for models defined in the gql schema", () => {
-    const services = buildServices(schemaPath);
+    const services: any = buildServices(schemaPath);
+    expect(services.User).toBeDefined();
+});
+
+describe("CRUD test", () => {
+
+    test.todo("it should create data and save to db");
+
+    test.todo("it should find data in db");
+
+    test.todo("it should update data in db");
+
+    test.todo("it should delete data matching the params in the db");
+
 });
