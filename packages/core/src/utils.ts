08f1d29ecb1287e46e3aba9383a4c91a48181176
Eunovo <eunovo9@gmail.com>
2020-12-28 21:58:50 +0100

    Extract metdata from description

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -9,3 +9,41 @@ export function extractModelsFrom(gqlSchema: GraphQLSchema): GraphQLObjectType[]
             return [...prev, node];
         }, []);
 }
+
+interface Metadata {
+    name: string;
+    value: any;
+}
+
+export function extractMetadata(description: string): Metadata[] {
+    const metadata: Metadata[] = [];
+    description.split("\n").forEach((token) => {
+        token.split(" ").forEach((token) => {
+            if (!token.startsWith('@')) {
+                return;
+            }
+
+            const startOfValue = token.indexOf('(');
+            if (startOfValue === -1) {
+                metadata.push({
+                    name: token.substring(1),
+                    value: true
+                });
+                return;
+            }
+
+            let name = token.substring(1, startOfValue);
+            let value: any = token.substring(startOfValue + 1, token.indexOf(')'));
+
+            if (value.startsWith("'") || value.startsWith('"')) {
+                value = value.substring(1, value.length - 1);
+            } else if (value === 'true' || value === 'false') {
+                value = Boolean(value);
+            }
+
+            metadata.push({ name, value });
+        });
+    });
+
+    return metadata;
+}
