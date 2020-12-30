6460c5ef16e3c5539067a56d637e511f53dc2130
Eunovo <eunovo9@gmail.com>
2020-12-30 23:46:04 +0100

    Test username and password authentication plugin

---
+++ packages/core/test/authentication.test.ts
@@ -0,0 +1,53 @@
+import "jest";
+import { buildSchema } from "graphql";
+import { extractModelsFrom, Service, UsernamePasswordAuthPlugin } from "../src";
+
+
+describe("test authentication plugin", () => {
+    test("it should hash password on create and provide an 'authenticate' method ", async () => {
+        const gqlSchemaString = `
+        """
+        @model
+        @usernamepasswordauth
+        """
+        type User {
+            """ @username """
+            username: String!
+            """ @password """
+            password: String!
+        }
+        `;
+        const gqlSchema = buildSchema(gqlSchemaString);
+        const User = extractModelsFrom(gqlSchema)[0];
+
+        const username = 'test';
+        const password = 'password';
+        let testUser: any;
+
+        const repo: any = {
+            findOne: async () => {
+                return testUser;
+            }
+        };
+
+        const create = async function (this: Service, data: any) {
+            [testUser] = await this.runPreMiddleware('create', data);
+            expect(testUser.password).not.toEqual(password);
+        }
+
+        let service: Service = new Service();
+        Object.defineProperty(service, 'create', {
+            value: create,
+            writable: true,
+            configurable: true,
+            enumerable: false
+        });
+
+        const authService = UsernamePasswordAuthPlugin(User, repo, service);
+
+        await authService.create({ username, password });
+        await authService.authenticate(username, password);
+
+        expect.assertions(1);
+    });
+});
