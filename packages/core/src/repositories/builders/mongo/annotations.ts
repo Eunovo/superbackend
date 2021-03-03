0ae6e6139b73517b946d0e7a4ba08f5e3e7d5d85
Eunovo <eunovo9@gmail.com>
2021-03-03 06:49:55 +0100

    Define DB supported annotations

---
+++ packages/core/src/repositories/builders/mongo/annotations.ts
@@ -0,0 +1,17 @@
+import { Metadata } from "../../../Model";
+import { DB_ANNOTATIONS } from "../../Repository";
+
+export const MONGO_ANNOTATIONS: { [P in DB_ANNOTATIONS] : (metadata: Metadata) => any } = {
+    default: ({ name, args }) => verifyMetadata({ name, args }, 1) && { default: args[0] },
+    unique: () => ({ unique: true }),
+    immutable: () => ({ immutable: true }),
+    index: () => ({ index: true })
+};
+
+function verifyMetadata({ name, args }: Metadata, length: number) {
+    if (args.length !== length) {
+        throw new Error(`${name} expects ${length} arguments but got ${args.length}`);
+    }
+
+    return true;
+}
