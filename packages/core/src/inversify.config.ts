aa02f6249f28334b474e42c0da2f60521e0aa891
Eunovo <eunovo9@gmail.com>
2022-01-12 06:39:57 +0100

    Inject observable object

--- packages/core/src/inversify.config.ts
+++ packages/core/src/inversify.config.ts
@@ -1,5 +1,7 @@
 import { Container } from "inversify";
 import "reflect-metadata";
+import { Observable } from "./Observable";
 
 const container = new Container();
+container.bind(Observable).toConstantValue(new Observable());
 export default container;
