78f42071a8dadcf8b1fec12c044ce5c87ee01298
Eunovo <eunovo9@gmail.com>
2022-01-12 06:41:05 +0100

    TODO Relationships

--- packages/core/src/plugins/relationships/RelationshipPlugin.ts
+++ packages/core/src/plugins/relationships/RelationshipPlugin.ts
@@ -1,45 +1,45 @@
-import { MapAll, Models, Repositories } from "../../utils";
-import { CRUDService } from "../../crud";
-import { Plugin } from "../Plugin";
+// import { MapAll, Models, Repositories } from "../../utils";
+// import { CRUDService } from "../../crud";
+// import { Plugin } from "../Plugin";
 
-export class RelationshipPlugin extends Plugin {
+// export class RelationshipPlugin extends Plugin {
 
-    transformServices(models: Models, _repos: Repositories, services: MapAll<any, CRUDService | undefined>) {
-        Object.values(models)
-            .forEach(model => {
-                const { name, fields } = model;
-                const service = services[name];
+//     transformServices(models: Models, _repos: Repositories, services: MapAll<any, CRUDService | undefined>) {
+//         Object.values(models)
+//             .forEach(model => {
+//                 const { name, fields } = model;
+//                 const service = services[name];
 
-                Object.values(fields)
-                    .forEach(field => {
-                        if (!field.foreignModel) return;
-                        const foreignService = services[field.foreignModel];
+//                 Object.values(fields)
+//                     .forEach(field => {
+//                         if (!field.foreignModel) return;
+//                         const foreignService = services[field.foreignModel];
 
-                        if (!foreignService)
-                            throw new Error(
-                                `CRUDService for ${field.foreignModel} ` +
-                                `must exist before relationships can be formed`
-                            );
+//                         if (!foreignService)
+//                             throw new Error(
+//                                 `CRUDService for ${field.foreignModel} ` +
+//                                 `must exist before relationships can be formed`
+//                             );
 
-                        const fetchForeigners = async (args: any) => {
-                            const { name, foreignKey } = field;
-                            const input = args.input;
-                            if (!foreignKey || !input[name]) return;
+//                         const fetchForeigners = async (args: any) => {
+//                             const { name, foreignKey } = field;
+//                             const input = args.input;
+//                             if (!foreignKey || !input[name]) return;
 
-                            const foreigner = await foreignService
-                                .findOne({ [foreignKey]: input[name] }, args.context);
-                            args._foreign = {
-                                ...(args.foreign || {}),
-                                [name]: foreigner
-                            }
-                        };
+//                             const foreigner = await foreignService
+//                                 .findOne({ [foreignKey]: input[name] }, args.context);
+//                             args._foreign = {
+//                                 ...(args.foreign || {}),
+//                                 [name]: foreigner
+//                             }
+//                         };
 
-                        service?.pre(
-                            ['create', 'updateOne', 'updateMany'],
-                            fetchForeigners
-                        );
-                    });
-            });
-    }
+//                         service?.pre(
+//                             ['create', 'updateOne', 'updateMany'],
+//                             fetchForeigners
+//                         );
+//                     });
+//             });
+//     }
 
-}
+// }
--- packages/core/src/plugins/relationships/index.ts
+++ packages/core/src/plugins/relationships/index.ts
@@ -1,1 +1,1 @@
-export * from './RelationshipPlugin';
+
